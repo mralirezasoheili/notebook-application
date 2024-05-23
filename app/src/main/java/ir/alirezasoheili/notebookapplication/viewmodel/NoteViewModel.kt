3fa600a98b1727a029b5c153f590defdda0cf3e8
Alireza Soheili <alirezasoheili1401@gmail.com>
2024-05-23 22:07:43 +0330

    Add ViewModel

---
+++ app/src/main/java/ir/alirezasoheili/notebookapplication/viewmodel/NoteViewModel.kt
@@ -0,0 +1,40 @@
+package ir.alirezasoheili.notebookapplication.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import ir.alirezasoheili.notebookapplication.model.Note
+import ir.alirezasoheili.notebookapplication.repository.NoteRepository
+import kotlinx.coroutines.launch
+
+class NoteViewModel(
+    app: Application, private val repository: NoteRepository
+) : AndroidViewModel(app) {
+    fun addNote(note: Note) {
+        viewModelScope.launch {
+            repository.insertNote(note)
+        }
+    }
+
+    fun updateNote(note: Note) {
+        viewModelScope.launch {
+            repository.updateNote(note)
+        }
+    }
+
+    fun deleteNote(note: Note) {
+        viewModelScope.launch {
+            repository.deleteNote(note)
+        }
+    }
+
+    fun deleteAllNotes() {
+        viewModelScope.launch { repository.deleteAllNotes() }
+    }
+
+    fun getAllNotes() = repository.getAllNotes()
+
+    fun searchNote(query: String?) = repository.searchNote(query)
+
+
+}
---
+++ app/src/main/java/ir/alirezasoheili/notebookapplication/viewmodel/NoteViewModelFactory.kt
@@ -0,0 +1,18 @@
+package ir.alirezasoheili.notebookapplication.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import ir.alirezasoheili.notebookapplication.repository.NoteRepository
+
+class NoteViewModelFactory(
+    private val application: Application, private val repository: NoteRepository
+) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(NoteViewModel::class.java)) {
+            return NoteViewModel(application, repository) as T
+        }
+        throw IllegalArgumentException("")
+    }
+}
