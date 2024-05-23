b4dea4195fa99fc970ad8e0eb2c9d0e30100612a
Alireza Soheili <alirezasoheili1401@gmail.com>
2024-05-23 21:32:24 +0330

    Add Dao

---
+++ app/src/main/java/ir/alirezasoheili/notebookapplication/database/NoteDao.kt
@@ -0,0 +1,31 @@
+package ir.alirezasoheili.notebookapplication.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import ir.alirezasoheili.notebookapplication.model.Note
+
+@Dao
+interface NoteDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(note: Note)
+
+    @Update
+    suspend fun update(note: Note)
+
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Query("DELETE FROM notes_tbl")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM notes_tbl ORDER BY note_id DESC")
+    fun getAllNotes(): LiveData<List<Note>>
+
+    @Query("SELECT * FROM notes_tbl WHERE note_title LIKE :query OR note_body LIKE :query ORDER BY note_id DESC")
+    fun search(query: String?): LiveData<List<Note>>
+}
