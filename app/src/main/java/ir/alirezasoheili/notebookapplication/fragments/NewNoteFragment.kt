0ce06e5f749358126431de7d916228e84b9fc4f6
Alireza Soheili <alirezasoheili1401@gmail.com>
2024-05-25 17:35:51 +0330

    Add functionalities to New Note Fragment

--- app/src/main/java/ir/alirezasoheili/notebookapplication/fragments/NewNoteFragment.kt
+++ app/src/main/java/ir/alirezasoheili/notebookapplication/fragments/NewNoteFragment.kt
@@ -4,16 +4,66 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import ir.alirezasoheili.notebookapplication.MainActivity
 import ir.alirezasoheili.notebookapplication.R
+import ir.alirezasoheili.notebookapplication.adapter.NoteAdapter
+import ir.alirezasoheili.notebookapplication.databinding.FragmentNewNoteBinding
+import ir.alirezasoheili.notebookapplication.model.Note
+import ir.alirezasoheili.notebookapplication.viewmodel.NoteViewModel
 
-class NewNoteFragment : Fragment() {
+class NewNoteFragment : Fragment(R.layout.fragment_new_note) {
 
+    private var _binding: FragmentNewNoteBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var noteViewModel: NoteViewModel
+
+    private lateinit var noteAdapter: NoteAdapter
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_note, container, false)
+        _binding = FragmentNewNoteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        noteViewModel = (activity as MainActivity).noteViewModel
+
+        // add click listener to floating action button
+        binding.fabSaveNote.setOnClickListener {
+            if (saveNote()) {
+                Toast.makeText(
+                    it.context, "Note Saved Successfully.", Toast.LENGTH_SHORT
+                ).show()
+                backToHomeFragment(it)
+            } else {
+                Toast.makeText(
+                    it.context, "Please Enter Note Title First!", Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun backToHomeFragment(view: View) {
+        view.findNavController().navigate(
+            R.id.action_newNoteFragment_to_homeFragment
+        )
+    }
+
+    private fun saveNote(): Boolean {
+        val noteTitle = binding.etNoteTitle.text.toString().trim()
+        val noteBody = binding.etNoteBody.text.toString().trim()
+
+        return if (noteTitle.isNotEmpty()) {
+            noteViewModel.addNote(Note(0, noteTitle, noteBody))
+            true
+        } else {
+            false
+        }
     }
 }
