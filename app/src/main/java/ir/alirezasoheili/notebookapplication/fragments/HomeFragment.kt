4b0415b1f1a964e14a58162f42ce188de9ee10b8
Alireza Soheili <alirezasoheili1401@gmail.com>
2024-05-25 17:17:17 +0330

    Change inflater in Home Fragment (onCreateView)

--- app/src/main/java/ir/alirezasoheili/notebookapplication/fragments/HomeFragment.kt
+++ app/src/main/java/ir/alirezasoheili/notebookapplication/fragments/HomeFragment.kt
@@ -34,7 +34,7 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        _binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
