1b95966f6ef41767537d1c48f2e7402b27c2d2c0
Alireza Soheili <alirezasoheili1401@gmail.com>
2024-05-25 18:39:04 +0330

    Add functionalities to MainActivity

--- app/src/main/java/ir/alirezasoheili/notebookapplication/MainActivity.kt
+++ app/src/main/java/ir/alirezasoheili/notebookapplication/MainActivity.kt
@@ -2,9 +2,12 @@ package ir.alirezasoheili.notebookapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import ir.alirezasoheili.notebookapplication.database.NoteDatabase
 import ir.alirezasoheili.notebookapplication.databinding.ActivityMainBinding
+import ir.alirezasoheili.notebookapplication.repository.NoteRepository
 import ir.alirezasoheili.notebookapplication.viewmodel.NoteViewModel
+import ir.alirezasoheili.notebookapplication.viewmodel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,7 +16,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpViewModel()
+    }
 
+    private fun setUpViewModel() {
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelFactory = NoteViewModelFactory(application, noteRepository)
+        noteViewModel = ViewModelProvider(this, viewModelFactory)[NoteViewModel::class.java]
     }
 }
