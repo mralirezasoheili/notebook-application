ad61c5e5d486ecdd6493c06d3e63b66dc4afd1f6
Alireza Soheili <alirezasoheili1401@gmail.com>
2024-05-25 17:09:29 +0330

    Add functionalities to Home Fragment

--- app/src/main/java/ir/alirezasoheili/notebookapplication/MainActivity.kt
+++ app/src/main/java/ir/alirezasoheili/notebookapplication/MainActivity.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import ir.alirezasoheili.notebookapplication.databinding.ActivityMainBinding
+import ir.alirezasoheili.notebookapplication.viewmodel.NoteViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    lateinit var noteViewModel: NoteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
--- app/src/main/java/ir/alirezasoheili/notebookapplication/fragments/HomeFragment.kt
+++ app/src/main/java/ir/alirezasoheili/notebookapplication/fragments/HomeFragment.kt
@@ -12,24 +12,57 @@ import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import ir.alirezasoheili.notebookapplication.MainActivity
 import ir.alirezasoheili.notebookapplication.R
+import ir.alirezasoheili.notebookapplication.adapter.NoteAdapter
+import ir.alirezasoheili.notebookapplication.databinding.FragmentHomeBinding
+import ir.alirezasoheili.notebookapplication.model.Note
+import ir.alirezasoheili.notebookapplication.viewmodel.NoteViewModel
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var noteViewModel: NoteViewModel
+
+    private lateinit var noteAdapter: NoteAdapter
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        noteViewModel = (activity as MainActivity).noteViewModel
+        setUpRecyclerView()
+
+        // add click listener to floating action button
+        binding.fabAddNote.setOnClickListener {
+            it.findNavController().navigate(
+                R.id.action_homeFragment_to_newNoteFragment
+            )
+        }
+
+        setUpMenu()
+    }
+
+    private fun setUpMenu() {
         val menuHost: MenuHost = requireActivity()
-        menuHost.addMenuProvider(object : MenuProvider {
+        menuHost.addMenuProvider(getMenuProvider(), viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
+    private fun getMenuProvider(): MenuProvider {
+        return object : MenuProvider {
             override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
                 menuInflater.inflate(R.menu.home_menu, menu)
+                setUpSearchView(menu)
             }
 
             override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
@@ -39,8 +72,74 @@ class HomeFragment : Fragment() {
                         return true
                     }
                 }
+                return false
+            }
+        }
+    }
+
+    private fun setUpSearchView(menu: Menu) {
+        val menuItem: MenuItem = menu.findItem(R.id.menu_search)
+        val menuSearch = menuItem.actionView as SearchView
+        menuSearch.isSubmitButtonEnabled = false
+        menuSearch.setOnQueryTextListener(getQueryTextListener())
+    }
+
+    private fun getQueryTextListener(): SearchView.OnQueryTextListener {
+        return object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+//                searchNote(query)
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText != null) {
+                    searchNote(newText)
+                }
                 return true
             }
-        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+        }
     }
+
+    private fun searchNote(query: String?) {
+        noteViewModel.searchNote(query).observe(
+            this
+        ) { list ->
+            noteAdapter.differ.submitList(list)
+        }
+    }
+
+    private fun setUpRecyclerView() {
+        noteAdapter = NoteAdapter()
+        binding.recyclerView.apply {
+            layoutManager = StaggeredGridLayoutManager(
+                2, StaggeredGridLayoutManager.VERTICAL
+            )
+            setHasFixedSize(true)
+            adapter = noteAdapter
+        }
+        activity?.let {
+            noteViewModel.getAllNotes().observe(viewLifecycleOwner) { note ->
+                noteAdapter.differ.submitList(note)
+                updateUI(note)
+            }
+        }
+    }
+
+    private fun updateUI(note: List<Note>?) {
+        if (note != null) {
+            if (note.isNotEmpty()) {
+                binding.cardView.visibility = View.GONE
+                binding.recyclerView.visibility = View.VISIBLE
+            } else {
+                binding.cardView.visibility = View.VISIBLE
+                binding.recyclerView.visibility = View.GONE
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
 }
